bee9a424b3c6261e90d473e457c16da523aff985
Maximo Mena <maximo.mena@semanticbits.com>
2020-01-28 00:12:42 -0700

    Format code

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,16 @@
 port module Main exposing (..)
 
 import Browser
-import Html exposing (Html)
-import Element exposing (Element, el, text, column, row, alignRight, fill, width, rgb255, spacing, centerX, centerY, padding)
+import Delay
+import Element exposing (Element, alignRight, centerX, centerY, column, el, fill, padding, rgb255, row, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
-import Random
-import Delay
+import Html exposing (Html)
 import Json.Encode as E
+import Random
+
 
 
 ---- PORTS ----
@@ -18,45 +19,47 @@ import Json.Encode as E
 port play : E.Value -> Cmd msg
 
 
+
 ---- MODEL ----
 
 
 type alias Model =
-    { screen: Screen
-    , status: Status
-    , note: Note
-    , show: Bool
+    { screen : Screen
+    , status : Status
+    , note : Note
+    , show : Bool
     }
 
 
 type Screen
-  = Home
-  | Game
+    = Home
+    | Game
 
 
 type Status
-  = Start
-  | Play
-  | Match
-  | Fail
-  | Win
+    = Start
+    | Play
+    | Match
+    | Fail
+    | Win
 
 
 type Note
-  = Do
-  | Re
-  | Mi
-  | Fa
-  | Sol
-  | La
-  | Si
+    = Do
+    | Re
+    | Mi
+    | Fa
+    | Sol
+    | La
+    | Si
 
 
 init : ( Model, Cmd Msg )
 init =
-      ( Model Game Start Do True
-      , Random.generate UpdateNote noteGenerator
-      )
+    ( Model Game Start Do True
+    , Random.generate UpdateNote noteGenerator
+    )
+
 
 
 ---- UPDATE ----
@@ -82,6 +85,7 @@ update msg model =
                 ( { model | show = True }
                 , Cmd.batch [ showMainNote, playNote note ]
                 )
+
             else
                 ( model
                 , playNote note
@@ -91,40 +95,42 @@ update msg model =
             ( model
             , Random.generate UpdateNote noteGenerator
             )
-            
+
         UpdateNote newNote ->
             ( Model
                 model.screen
                 model.status
-                newNote 
+                newNote
                 False
             , Cmd.none
             )
 
 
+
 ---- COMMANDS ----
 
 
-playNote: Note -> Cmd Msg
-playNote note = 
-    play ( E.string ( viewNote note ) )
+playNote : Note -> Cmd Msg
+playNote note =
+    play (E.string (viewNote note))
 
 
-showMainNote: Cmd Msg
+showMainNote : Cmd Msg
 showMainNote =
     Delay.after 500 Delay.Millisecond Shuffle
 
 
 noteGenerator : Random.Generator Note
 noteGenerator =
-  Random.uniform Do
-    [ Re
-    , Mi
-    , Fa
-    , Sol
-    , La
-    , Si
-    ]
+    Random.uniform Do
+        [ Re
+        , Mi
+        , Fa
+        , Sol
+        , La
+        , Si
+        ]
+
 
 
 ---- VIEW ----
@@ -133,28 +139,28 @@ noteGenerator =
 view : Model -> Html Msg
 view model =
     Element.layout []
-        ( el 
+        (el
             [ centerX
             , centerY
             ]
-            ( column []
+            (column []
                 [ el
                     [ Font.color (rgb255 64 64 64)
                     , Font.size 48
                     , centerX
                     , padding 8
                     ]
-                    ( text "Hearing Trainer" )
+                    (text "Hearing Trainer")
                 , el
                     [ centerX
                     , padding 8
                     ]
-                    ( mainNote model )
+                    (mainNote model)
                 , el
                     [ centerX
                     , padding 8
                     ]
-                    ( row [ spacing 8 ]
+                    (row [ spacing 8 ]
                         [ musicNote Do
                         , musicNote Re
                         , musicNote Mi
@@ -166,9 +172,9 @@ view model =
                     )
                 , el
                     [ centerX
-                    , padding 8 
+                    , padding 8
                     ]
-                    ( statusMsg ( viewStatus model.status ) )
+                    (statusMsg (viewStatus model.status))
                 ]
             )
         )
@@ -177,23 +183,45 @@ view model =
 viewStatus : Status -> String
 viewStatus status =
     case status of
-        Start -> "Push the yellow buttons to become familiar with the sounds of the music notes. When you're ready, push the start button to beginn playing."
-        Play -> "Push the card with the question mark to listen to a musical note, then select the correct note from the multiple choices."
-        Match -> "Very well! Now guess the next note."
-        Fail -> "Try again! That was not the correct note."
-        Win -> "Excellent! You won."
-     
+        Start ->
+            "Push the yellow buttons to become familiar with the sounds of the music notes. When you're ready, push the start button to beginn playing."
+
+        Play ->
+            "Push the card with the question mark to listen to a musical note, then select the correct note from the multiple choices."
+
+        Match ->
+            "Very well! Now guess the next note."
+
+        Fail ->
+            "Try again! That was not the correct note."
+
+        Win ->
+            "Excellent! You won."
+
 
 viewNote : Note -> String
 viewNote note =
     case note of
-        Do -> "Do"
-        Re -> "Re"
-        Mi -> "Mi"
-        Fa -> "Fa"
-        Sol -> "Sol"
-        La -> "La"
-        Si -> "Si"
+        Do ->
+            "Do"
+
+        Re ->
+            "Re"
+
+        Mi ->
+            "Mi"
+
+        Fa ->
+            "Fa"
+
+        Sol ->
+            "Sol"
+
+        La ->
+            "La"
+
+        Si ->
+            "Si"
 
 
 mainNote : Model -> Element Msg
@@ -206,7 +234,14 @@ mainNote model =
             , padding 30
             ]
             { onPress = Just CheckNote
-            , label = text ( if model.show then viewNote model.note else "?" )
+            , label =
+                text
+                    (if model.show then
+                        viewNote model.note
+
+                     else
+                        "?"
+                    )
             }
         )
 
@@ -220,8 +255,8 @@ musicNote note =
             , Border.rounded 3
             , padding 30
             ]
-            { onPress = Just ( SelectNote note )
-            , label = text ( viewNote note )
+            { onPress = Just (SelectNote note)
+            , label = text (viewNote note)
             }
         )
 
@@ -230,9 +265,10 @@ statusMsg : String -> Element Msg
 statusMsg status =
     row []
         [ el []
-            ( text status )
+            (text status)
         ]
-        
+
+
 
 ---- PROGRAM ----
 
